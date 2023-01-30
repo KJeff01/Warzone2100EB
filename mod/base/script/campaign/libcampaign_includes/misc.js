3652e001efe0dc36e7f526fb36f27f96797ea717
KJeff01 <kjeff1997@gmail.com>
2023-01-30 16:40:05 -0600

    Update scripts to master and fix rocket cyborgs
    Patch in some script updates.
    Fix two rocket cyborgs having the reload speed of an Assault Gun.

--- mod/base/script/campaign/libcampaign_includes/misc.js
+++ mod/base/script/campaign/libcampaign_includes/misc.js
@@ -643,14 +643,6 @@ function __camAiPowerReset()
 	}
 }
 
-function __camSetOffworldLimits()
-{
-	// These are the only structures that do not get
-	// auto-disabled by the engine in off-world missions.
-	setStructureLimits("A0CommandCentre", 0, CAM_HUMAN_PLAYER);
-	setStructureLimits("A0ComDroidControl", 0, CAM_HUMAN_PLAYER);
-}
-
 function __camGetExpRangeLevel()
 {
 	let ranks = {
